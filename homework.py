4e7a7eddf88d6340ae558fbc0b61a0824deeb1d0
0001srujan <0001srujan@gmail.com>
2025-11-25 19:04:54 +0530

    calculation_1

---
+++ homework.py
@@ -0,0 +1,7 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the secound number: "))
+
+print("addition:", a + b)
+print("substraction:", a - b)
+print("multiplication:", a * b)
+print("division:", a / b)
